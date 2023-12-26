95dc36c06b8a75a1b5f36203184d6f4f6a2cd31c
Dev Royale <unityekeoba18@gmail.com>
2023-12-27 00:13:09 +0100

    created components folder

---
+++ components/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar(){
+    return(
+        <nav>
+            <div className="navbar">
+                <h3>Favor Bless Media</h3>
+                <ul className="nav-items">
+                    <li>Home</li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
