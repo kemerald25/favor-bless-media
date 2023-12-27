f0214203b2407db1dd8f3e1c047e64b90ebd266f
Dev Royale <126842942+kemerald25@users.noreply.github.com>
2023-12-27 17:46:43 +0100

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from "../public/components/Navbar";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
+import Service from "../pages/Service";
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
