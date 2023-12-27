7643369f384ad213ef3fe9d8053767e6939d6d63
Dev Royale <unityekeoba18@gmail.com>
2023-12-27 10:45:18 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Contact from "../pages/Contact";
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
